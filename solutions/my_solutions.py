1f75289779cc3175a790fddeb5f2f8a5d1ae34e7
Roman <Roman_Selianko@epam.com>
2019-07-23 18:58:52 +0400

    reformatting docs

--- solutions/my_solutions.py
+++ solutions/my_solutions.py
@@ -128,10 +128,12 @@ def last_stone_weight(stones: List[int]) -> int:
 
 def min_steps(n: int) -> int:
     """
-    Initially on a notepad only one character 'A' is present. You can perform two operations on this notepad for each step:
+    Initially on a notepad only one character 'A' is present.
+    You can perform two operations on this notepad for each step:
     Copy All: You can copy all the characters present on the notepad (partial copy is not allowed).
     Paste: You can paste the characters which are copied last time.
-    Given a number n. You have to get exactly n 'A' on the notepad by performing the minimum number of steps permitted. Output the minimum number of steps to get n 'A'.
+    Given a number n. You have to get exactly n 'A' on the notepad by performing the minimum number of steps permitted.
+    Output the minimum number of steps to get n 'A'.
     :param n: length of line that contains 'A'
     :return: minimum number of steps that we need to achieve desirable length
     Solution: we just should find biggest divisor of N
