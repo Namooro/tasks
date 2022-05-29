57721ead94819df76d23d744ddf9d8650baebbde
Namooro <romanselyanko@gmail.com>
2022-05-30 01:09:23 +0400

    cleanup

--- binary_tree/binary_tree.py
+++ binary_tree/binary_tree.py
@@ -9,44 +9,44 @@ class TreeNode:
         self.right = None
 
 
-class TreeSolutions:
-    def invert_tree(root: TreeNode) -> TreeNode:
-        """
-        Inverting the btree via recursion
-        :param: root of binary tree:
-        :returns: inverted binary tree
-        """
-        if root is None:
-            return 0
-        root.left, root.right = root.right, root.left
-        if root.left:
-            TreeSolutions.invert_tree(root.left)
-        if root.right:
-            TreeSolutions.invert_tree(root.right)
-        return root
+def invert_tree(root: TreeNode) -> TreeNode:
+    """
+    Inverting the btree via recursion
+    :param: root of binary tree:
+    :returns: inverted binary tree
+    """
+    if root is None:
+        return 0
+    root.left, root.right = root.right, root.left
+    if root.left:
+        invert_tree(root.left)
+    if root.right:
+        invert_tree(root.right)
+    return root
 
-    def max_level_sum(root: TreeNode) -> int:
-        """
-        return s maximum sum of a level in Binary Tree
-        param: root of the btree
-        returns max sum of level
-        """
-        if root is None:
-            return 0
-        result = root.data
-        q = deque()
-        q.append(root)
-        while len(q) > 0:
-            count = len(q)
-            summary = 0
-            while count > 0:
-                temp = q.popleft()
-                summary += temp.data
-                if temp.left is not None:
-                    q.append(temp.left)
-                if temp.right is not None:
-                    q.append(temp.right)
-                count -= 1
-            result = max(summary, result)
 
-        return result
+def max_level_sum(root: TreeNode) -> int:
+    """
+    return s maximum sum of a level in Binary Tree
+    param: root of the btree
+    returns max sum of level
+    """
+    if root is None:
+        return 0
+    result = root.data
+    q = deque()
+    q.append(root)
+    while len(q) > 0:
+        count = len(q)
+        summary = 0
+        while count > 0:
+            temp = q.popleft()
+            summary += temp.data
+            if temp.left is not None:
+                q.append(temp.left)
+            if temp.right is not None:
+                q.append(temp.right)
+            count -= 1
+        result = max(summary, result)
+
+    return result
